d6a4d039c896def53b7b974aa33813069e745f2a
j- <j@skeoh.com>
2017-07-30 23:29:39 +1000

    Add GitHub ribbon
    Move heading to make room for it.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
+import Ribbon from './Ribbon';
 import Converter from './Converter';
+import './App.css';
 
 const App = () => (
 	<div className="App">
+		<Ribbon />
 		<h1>Tables</h1>
 		<Converter />
 	</div>
